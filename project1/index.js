73c6ae60ee9a45a3b454457740966e9723f3ecd6
alemprogramer <alemhhaisb@gmail.com>
2021-01-23 11:16:03 +0600

    first commit

---
+++ project1/index.js
@@ -0,0 +1,78 @@
+ let obj = [
+    {
+    "name": "Bangladesh",
+    "topLevelDomain": [
+    ".bd"
+    ],
+    "alpha2Code": "BD",
+    "alpha3Code": "BGD",
+    "callingCodes": [
+    "880"
+    ],
+    "capital": "Dhaka",
+    "altSpellings": [
+    "BD",
+    "People's Republic of Bangladesh",
+    "Gônôprôjatôntri Bangladesh"
+    ],
+    "region": "Asia",
+    "subregion": "Southern Asia",
+    "population": 161006790,
+    "latlng": [
+    24,
+    90
+    ],
+    "demonym": "Bangladeshi",
+    "area": 147570,
+    "gini": 32.1,
+    "timezones": [
+    "UTC+06:00"
+    ],
+    "borders": [
+    "MMR",
+    "IND"
+    ],
+    "nativeName": "Bangladesh",
+    "numericCode": "050",
+    "currencies": [
+    {
+    "code": "BDT",
+    "name": "Bangladeshi taka",
+    "symbol": "৳"
+    }
+    ],
+    "languages": [
+    {
+    "iso639_1": "bn",
+    "iso639_2": "ben",
+    "name": "Bengali",
+    "nativeName": "বাংলা"
+    }
+    ],
+    "translations": {
+    "de": "Bangladesch",
+    "es": "Bangladesh",
+    "fr": "Bangladesh",
+    "ja": "バングラデシュ",
+    "it": "Bangladesh",
+    "br": "Bangladesh",
+    "pt": "Bangladeche",
+    "nl": "Bangladesh",
+    "hr": "Bangladeš",
+    "fa": "بنگلادش"
+    },
+    "flag": "https://restcountries.eu/data/bgd.svg",
+    "regionalBlocs": [
+    {
+    "acronym": "SAARC",
+    "name": "South Asian Association for Regional Cooperation",
+    "otherAcronyms": [],
+    "otherNames": []
+    }
+    ],
+    "cioc": "BAN"
+    }
+    ]
+
+    console.log(obj[0].name);
+
---
+++ temperature/temp.js
@@ -0,0 +1,3 @@
+const temp = document.getElementById('temp');
+
+temp.innerHTML = '&#xf2c7'
